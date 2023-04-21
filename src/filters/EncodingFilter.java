0cbe61e95a388d60d555bed86d8dd0c45cd901c5
syuuya.watanabe3 <syuya1207@gmail.com>
2023-04-21 22:16:51 +0900

    ShowServlet.javaを作成

--- src/filters/EncodingFilter.java
+++ src/filters/EncodingFilter.java
@@ -35,9 +35,8 @@ public class EncodingFilter implements Filter {
      */
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         // TODO Auto-generated method stub
-        // place your code here
-
-        // pass the request along the filter chain
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
         chain.doFilter(request, response);
     }
 
