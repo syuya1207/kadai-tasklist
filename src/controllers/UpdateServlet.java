1b3c13356fd6482ab98f3e6c6587ada8854ce53a
syuuya.watanabe3 <syuya1207@gmail.com>
2023-04-22 19:03:40 +0900

    クラス名 Message から Task に変更

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Message;
+import models.Task;
 import utils.DBUtil;
 
 /**
@@ -38,7 +38,7 @@ public class UpdateServlet extends HttpServlet {
 
             // セッションスコープからメッセージのIDを取得して
             // 該当のIDのメッセージ1件のみをデータベースから取得
-            Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
+            Task m = em.find(Task.class, (Integer)(request.getSession().getAttribute("message_id")));
 
             // フォームの内容を各フィールドに上書き
 
